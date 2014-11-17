a44621872637b42571700617f55003db49d34a56
Stephen Leavitt <stephen.leavitt@sonyatv.com>
2014-11-17 18:24:44 +0000

    Refactored PharInstaller to be more like Composer's LibraryInstaller to include Windows support, and support for removeBinaries() as well.

--- src/PharInstaller.php
+++ src/PharInstaller.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace hco\ComposerPharInstaller;
-
 
 use Composer\Composer;
 use Composer\Installer\InstallerInterface;
@@ -23,27 +21,95 @@ class PharInstaller extends LibraryInstaller implements InstallerInterface
         );
     }
 
-    protected function installBinaries(PackageInterface $package)
+    protected function _getPharPackageInfo($package)
     {
-        $this->initializeBinDir();
-        parent::installBinaries($package);
-        $packageExtra = $package->getExtra();
+        $binPath = '';
+        $bin     = '';
+        $link    = '';
 
         $phars = glob($this->getInstallPath($package) . '/*.phar');
-        $pathToPhar = realpath(reset($phars));
 
-        if(isset($packageExtra['bin-name'])) {
-            $link = $this->binDir . '/' . $packageExtra['bin-name'];
-        } else {
-            $link = $this->binDir . '/' . basename($pathToPhar);
-        }
-        $relativeBin = $this->filesystem->findShortestPath($link, $pathToPhar);
+        if (!empty($phars)) {
+            $packageExtra = $package->getExtra();
 
-
-        if (false === symlink($relativeBin, $link)) {
-            throw new \ErrorException();
+            $binPath = realpath(reset($phars));
+            $bin = isset($packageExtra['bin-name']) ? $packageExtra['bin-name'] : basename($binPath);
+            $link = $this->binDir . '/' . $bin;
         }
 
-        @chmod($link, 0777 & ~umask());
+        return array(
+            'binPath' => $binPath,
+            'bin'     => $bin,
+            'link'    => $link
+        );
+    }
+
+    protected function installBinaries(PackageInterface $package)
+    {
+        parent::installBinaries($package);
+
+        // Extract the components of the returned array into PHP variables.
+        extract($this->_getPharPackageInfo($package));
+
+        // Install link
+        if (!empty($binPath)) {
+            $this->initializeBinDir();
+
+            if (file_exists($link)) {
+                if (is_link($link)) {
+                    // likely leftover from a previous install, make sure
+                    // that the target is still executable in case this
+                    // is a fresh install of the vendor.
+                    @chmod($link, 0777 & ~umask());
+                }
+                $this->io->write('    Skipped installation of bin '.$bin.' for package '.$package->getName().': name conflicts with an existing file');
+                return;
+            }
+            if (defined('PHP_WINDOWS_VERSION_BUILD')) {
+                // add unixy support for cygwin and similar environments
+                if ('.bat' !== substr($binPath, -4)) {
+                    file_put_contents($link, $this->generateUnixyProxyCode($binPath, $link));
+                    @chmod($link, 0777 & ~umask());
+                    $link .= '.bat';
+                    if (file_exists($link)) {
+                        $this->io->write('    Skipped installation of bin '.$bin.'.bat proxy for package '.$package->getName().': a .bat proxy was already installed');
+                    }
+                }
+                if (!file_exists($link)) {
+                    file_put_contents($link, $this->generateWindowsProxyCode($binPath, $link));
+                }
+            } else {
+                $cwd = getcwd();
+                try {
+                    // under linux symlinks are not always supported for example
+                    // when using it in smbfs mounted folder
+                    $relativeBin = $this->filesystem->findShortestPath($link, $binPath);
+                    chdir(dirname($link));
+                    if (false === symlink($relativeBin, $link)) {
+                        throw new \ErrorException();
+                    }
+                } catch (\ErrorException $e) {
+                    file_put_contents($link, $this->generateUnixyProxyCode($binPath, $link));
+                }
+                chdir($cwd);
+            }
+            @chmod($link, 0777 & ~umask());
+        }
+    }
+
+    protected function removeBinaries(PackageInterface $package)
+    {
+        parent::removeBinaries($package);
+
+        // Extract the components of the returned array into PHP variables.
+        extract($this->_getPharPackageInfo($package));
+
+        // Remove any existing links to PHAR
+        if (is_link($link) || file_exists($link)) {
+            $this->filesystem->unlink($link);
+        }
+        if (file_exists($link.'.bat')) {
+            $this->filesystem->unlink($link.'.bat');
+        }
     }
 }
